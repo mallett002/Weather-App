84db7e12fac81ece9727c93c6ef8c7d00dbf5095
William Mallett <mallett002@gmail.com>
2018-07-01 11:28:31 -0500

    Updated background color on City page

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './app/App';
 import './index.css';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 navigator.serviceWorker.getRegistrations().then(function(registrations) {
     for(let registration of registrations) {
      registration.unregister()
-   } })
+} })
 
 render(
-    <HashRouter>
+    <BrowserRouter>
         <App />
-    </HashRouter>,
+    </BrowserRouter>,
     document.getElementById('root')
 );
